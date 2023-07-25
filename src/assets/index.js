5d845dc782d4e9c4e7cf96b617b9c1666a183bd2
Sourabh Bhatt <sourabhbhatt825@gmail.com>
2023-07-26 01:11:24 +0530

    imported all files

---
+++ src/assets/index.js
@@ -0,0 +1,27 @@
+import logo from "./images/logo-portfolio.png";
+import bannerImg from "./images/MY_IMAGE.jpg";
+import projectOne from "./images/projects/amazon-clone.png";
+import projectTwo from "./images/projects/bg-changer.png";
+import projectThree from "./images/projects/counter-react-app.png";
+import projectFour from "./images/projects/iNeuron_Clone.png";
+import projectFive from "./images/projects/weather-app.png";
+import testimonialOne from "./images/testmonial/testimonialOne.jpg";
+import testimonialTwo from "./images/testmonial/testimonialTwo.jpeg";
+import testimonialThree from "./images/testmonial/testimonialThree.jpeg";
+import quote from "./images/testmonial/quote.png";
+import contactImg from "./images/contacts/1280px-Hand_Gesture_-_Hand_Shake_Vector.svg";
+
+export {
+  logo,
+  bannerImg,
+  projectOne,
+  projectTwo,
+  projectThree,
+  projectFour,
+  projectFive,
+  testimonialOne,
+  testimonialTwo,
+  testimonialThree,
+  quote,
+  contactImg,
+};
